46550fb186e1ddf0f0ca7a6b772e6d1be9c3deee
kasonchan <kasonl.chan@gmail.com>
2020-03-19 23:27:29 +0000

    3 Add application.conf, logback.xml and relocate actorsystem to App

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -1,11 +1,36 @@
-import Profile.system
+import Profile.{host, port, serviceName}
+import akka.Done
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.scaladsl.Behaviors
+import http.HttpService.{routes, start}
+
+import scala.util.{Failure, Success}
 
 /**
   * @author kasonchan
   * @since 2020-03-15
   */
 object App {
-  def main(args: Array[String]): Unit = {
+  private val rootBehavior = Behaviors.setup[Nothing] { context =>
+    import system.executionContext
+
     system.log.info("[STARTING]")
+    val futureBinding = start(host, port, routes, context.system)
+
+    futureBinding.onComplete {
+      case Success(binding) =>
+        system.log.info("[READY]")
+        Done
+      case Failure(ex) =>
+        system.log.error("[FATAL]", ex)
+        system.terminate()
+    }
+
+    Behaviors.empty
   }
+
+  implicit val system: ActorSystem[Nothing] =
+    ActorSystem[Nothing](rootBehavior, serviceName)
+
+  def main(args: Array[String]): Unit = {}
 }
