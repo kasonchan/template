1276c4133830153d5db21fedc1c47d7be1b8279e
kasonchan <kasonl.chan@gmail.com>
2020-03-28 14:58:01 +0000

    3 Refactor Guardian

--- src/main/scala/behaviors/Guardian.scala
+++ src/main/scala/behaviors/Guardian.scala
@@ -10,44 +10,57 @@ import protocol.status.{Fatal, Ready, Starting, Status}
   * @since 2020-03-21
   */
 object Guardian {
-  def apply(): Behavior[Message] = status(Starting)
+  def apply(): Behavior[Message] = starting()
 
-  private def status(currentStatus: Status): Behavior[Message] =
+  private def starting(currentStatus: Status = Starting): Behavior[Message] =
     Behaviors.setup[Message] { context =>
-      currentStatus match {
-        case Starting =>
-          Behaviors
-            .receiveMessage {
-              case Request(protocol.command.Activate, replyTo) =>
-                context.log.info("[{}]", Starting.toString.toUpperCase)
-                val web = context.spawn(Web(), "web")
-                web ! Request(protocol.command.Activate, context.self)
-                context.log
-                  .info(
-                    "[{}]-{}->[{}]",
-                    currentStatus.toString.toUpperCase,
-                    protocol.command.Activate.toString,
-                    Ready.toString.toUpperCase
-                  )
-                replyTo ! Response(currentStatus)
-                status(currentStatus)
-              case Request(protocol.command.Status, replyTo) =>
-                replyTo ! Response(currentStatus)
-                status(currentStatus)
-              case Response(responseStatus) =>
-                context.log
-                  .info("[{}]", responseStatus.toString.toUpperCase)
-                status(responseStatus)
-            }
+      Behaviors.receiveMessage {
+        case Request(protocol.command.Activate, replyTo) =>
+          context.log.info("[{}]", currentStatus.toString.toUpperCase)
+          val web = context.spawn(Web(), "web")
+          web ! Request(protocol.command.Activate, context.self)
+          context.log
+            .info(
+              "[{}]-{}->[{}]",
+              currentStatus.toString.toUpperCase,
+              protocol.command.Activate.toString,
+              Ready.toString.toUpperCase
+            )
+          replyTo ! Response(currentStatus)
+          Behaviors.same
+        case Request(protocol.command.Status, replyTo) =>
+          replyTo ! Response(currentStatus)
+          Behaviors.same
+        case Response(responseStatus) =>
+          context.log
+            .info("[{}]", responseStatus.toString.toUpperCase)
+          responseStatus match {
+            case Ready => ready()
+            case Fatal => fatal()
+            case _     => Behaviors.same
+          }
+      }
+    }
+
+  private def ready(currentStatus: Status = Ready): Behavior[Message] =
+    Behaviors.setup[Message] { context =>
+      Behaviors.receiveMessage {
+        case Request(protocol.command.Status, replyTo) =>
+          replyTo ! Response(currentStatus)
+          Behaviors.same
         case _ =>
-          Behaviors
-            .receiveMessage {
-              case Request(protocol.command.Status, replyTo) =>
-                replyTo ! Response(currentStatus)
-                status(currentStatus)
-              case _ =>
-                status(currentStatus)
-            }
+          Behaviors.same
+      }
+    }
+
+  private def fatal(currentStatus: Status = Fatal): Behavior[Message] =
+    Behaviors.setup[Message] { context =>
+      Behaviors.receiveMessage {
+        case Request(protocol.command.Status, replyTo) =>
+          replyTo ! Response(currentStatus)
+          Behaviors.same
+        case _ =>
+          Behaviors.same
       }
     }
 }
